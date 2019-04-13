405b514acbbd2f633894ef47b244fa887268bf17
Alan Cardero <alan.cardero@gmail.com>
2019-04-13 15:30:37 -0400

    Updates

---
+++ precache-manifest.2159850bb8999e2d4f675b75d49786fc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/acardero/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "a3e022e1f0008c5138c4",
+    "url": "/acardero/static/js/runtime~main.5473f403.js"
+  },
+  {
+    "revision": "1c73c12dc5df91e10dd4",
+    "url": "/acardero/static/js/main.c7211882.chunk.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/acardero/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "1c73c12dc5df91e10dd4",
+    "url": "/acardero/static/css/main.4fd49afe.chunk.css"
+  },
+  {
+    "revision": "99004905d4ab72e279fe871bfbc76971",
+    "url": "/acardero/index.html"
+  }
+];
